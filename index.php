e45182a5871912498fad274e8751999049793970
Parisxx <505825@vistacollege.nl>
2023-05-26 11:37:26 +0200

    h

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require('initialize.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,12 +12,24 @@
     <link rel="stylesheet" href="assets/css/landing.css">
 </head>
 <body>
+    <div class="nav">
+</div>
     <div class="logo">
         <a href="index.php"><img src="assets/img/logo.png"></a>
     </div>
 
-    <div class="landing">
-        about us
-    </div>
+    <div class="landing"><p class="text">
+    Welkom bij ons zelfgestarte bedrijf, 
+    waar we na zorgvuldig nadenken een uitdagende en 
+    waardevolle onderneming hebben ontwikkeld. 
+    Ons concept is geïnspireerd door de behoefte aan een 
+    professioneel platform waar bedrijven vacatures online 
+    kunnen plaatsen en freelancers kunnen worden ingehuurd 
+    voor diverse projecten. Met een focus op kwaliteit en een 
+    professionele aanpak streven wij ernaar om een platform te 
+    creëren dat de behoeften van zowel werkgevers als freelancers vervult. 
+    Ons doel is om een platform te bieden waarop bedrijven gemakkelijk 
+    gekwalificeerde freelancers kunnen vinden en samenwerken aan succesvolle projecten.
+</p></div>
 </body>
 </html>
