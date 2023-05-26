a3e72a3232d45bef9173aaf269a906986b8b1360
Parisxx <505825@vistacollege.nl>
2023-05-26 14:06:56 +0200

    f

--- index.php
+++ index.php
@@ -17,10 +17,19 @@ require('initialize.php');
         <a href="register.php" class="btn btn-link text-decoration-none">Register</a>
     </div>
 
+<div class="about">
     <div class="logo">
         <a href="index.php"><img src="assets/img/logo.png"></a>
     </div>
-
+    <div class="text">
+         <h3 class="title">About us</h3>
+         <p>Welkom bij Skillhub, het toonaangevende platform voor het verbinden van bedrijven met getalenteerde freelancers. Bij ons draait alles om het vervullen van de behoeften van zowel werkgevers als freelancers in de wereld van projectmatig werk. </p>
+         <img class="company" src="assets/img/bedrijf.png">
+         <p>Als zelfgestart bedrijf hebben we met passie en toewijding gewerkt aan de ontwikkeling van dit innovatieve concept. Ons doel is om een platform te creëren dat de manier waarop bedrijven hun freelance talent vinden en inhuren revolutioneert.</p>
+         <p>We begrijpen dat elk bedrijf uniek is en specifieke expertise vereist. Daarom bieden wij een breed scala aan freelance professionals met diverse vaardigheden en ervaringsniveaus. Of je nu een start-up bent die extra ondersteuning nodig heeft of een gevestigd bedrijf dat tijdelijk expertise zoekt, ons platform is ontworpen om aan al je eisen te voldoen.</p>
+         <br>
+    </div>
+</div>
 
 </body>
 </html>
