b0ce2dd566faba0c0be708f2f740f4cadaba0fcd
Parisxx <505825@vistacollege.nl>
2023-05-26 11:25:22 +0200

    f

--- index.php
+++ index.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Skillhub</title>
+    <link rel="stylesheet" href="assets/css/landing.css">
 </head>
 <body>
-    landing page
+    <div class="logo">
+        <a href="index.php"><img src="assets/img/logo.png"></a>
+    </div>
+
+    <div class="landing">
+        about us
+    </div>
 </body>
 </html>
