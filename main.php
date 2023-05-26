4d58263d21aaf76189cae7d311bb2d218299dd6d
= <=>
2023-05-26 11:37:11 +0200

    ws

--- main.php
+++ main.php
@@ -25,10 +25,29 @@ $row=mysqli_fetch_array($query);
             <a href="" class="btn btn-link text-decoration-none">Find Freelancers</a>
         </div>
         <div class="nav-info">
+            <a data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-light fa-plus"></i></a>
             <i class="fa-light fa-bell"></i>
             <img src="assets/pfp/<?php echo $row['pfp'];?>" class="image">
         </div>
     </div>
+    <!-- Modal -->
+<div class="modal" id="exampleModal" data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="staticBackdropLabel">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     <div class="maincontainer">
         <div class="col-sm-4">
             <div class="filter-tab">
