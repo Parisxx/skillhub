6b5c6884fe2925136c73cfaa74183c39d0d7dd0b
JayHuijbregts05 <505564@vistacollege.nl>
2023-06-01 08:53:47 +0100

    da

--- login.php
+++ login.php
@@ -60,12 +60,6 @@ and password='".md5($password)."'";
 
               <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
 
-              <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-              </div>
-
             </div>
 
             <div>
