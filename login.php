a58ae4ca9c217c4353f31d7c381a77c660cb1fc3
JayHuijbregts05 <505564@vistacollege.nl>
2023-06-01 08:53:07 +0100

    ddada

--- login.php
+++ login.php
@@ -34,21 +34,51 @@ and password='".md5($password)."'";
         <input type="submit" value="Submit" name="submit" class="submit">
     </form>
 
-    <div class="screen-1"><svg class="logo" xmlns="assets/img/logo.png"  version="1.1" width="300" height="300" viewbox="0 0 640 480" xml:space="preserve">
-    <div namespace="email"><label for="email">Email Address</label>
-        <div class="sec-2">
-            <ion-icon name="mail"></ion-icon><input type="email" name="email" placeholder="Username@gmail.com" />
-        </div>
-    </div>
-    <div class="password"><label for="password">Password</label>
-        <div class="sec-2">
-            <ion-icon name="lock-closed-outline"></ion-icon><input class="password" type="password" name="password" placeholder="············" />
-            <ion-icon class="show-hide" name="eye-outline"></ion-icon>
-        </div>
-    </div><button class="submit">Login </button>
-    <div class="footer"><span>Sign up</span><span>Forgot Password?</span></div>
-</div>
+    <section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
 
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Please enter your login and password!</p>
+
+              <div class="form-outline form-white mb-4">
+                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+                <label class="form-label" for="typeEmailX">Email</label>
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                <label class="form-label" for="typePasswordX">Password</label>
+              </div>
+
+              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+              <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+              </div>
+
+            </div>
+
+            <div>
+              <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 </body>
 </html>
