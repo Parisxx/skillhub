839bd41bd4c3ff18179bdbe27c2ffd708676296b
Parisxx <505825@vistacollege.nl>
2023-06-01 12:50:18 +0200

    g

--- register.php
+++ register.php
@@ -49,7 +49,7 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Skillhub | Register</title>
-    <link rel="stylesheet" href="assets/css/register.css">
+    <link rel="stylesheet" href="assets/css/login-register.css">
 </head>
 <body>
     <h1 class="text-center">Register</h1>
