3b9e93d483006ff48c07b1399539a030a1269fa7
Parisxx <505825@vistacollege.nl>
2023-06-02 11:16:42 +0200

    g

--- profile.php
+++ profile.php
@@ -1,3 +1,11 @@
+<?php
+require('initialize.php');
+require('auth.php'); 
+
+$query=mysqli_query($db,"SELECT * FROM user WHERE email = '".$_SESSION['email']."'")or die(mysqli_error());
+$row2=mysqli_fetch_array($query);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +13,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Skillhub | Profile</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+<!-- navbar -->
+    <div class="nav">
+        <div class="nav-logo">
+            <a href="main.php"><img class="logo" src="assets/img/logo.png"></a>
+        </div>
+        <div class="nav-links">
+            <a href="" class="btn btn-link text-decoration-none">Find Projects</a>
+            <a href="" class="btn btn-link text-decoration-none">Find Freelancers</a>
+        </div>
+        <div class="nav-info">
+            <a data-bs-toggle="modal" data-bs-target="#exampleModal" class="test"><i class="fa-light fa-plus"></i></a>
+            <i class="fa-light fa-bell"></i>
+            <a href="profile.php"><img src="assets/pfp/<?php echo $row2['pfp'];?>" class="image"></a>
+        </div>
+    </div>
+
 </body>
 </html>
