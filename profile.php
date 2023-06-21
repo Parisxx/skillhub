be27893be4b9bf8e6b85c9a8460a41670479f48f
= <=>
2023-06-21 09:30:06 +0200

    dw

--- profile.php
+++ profile.php
@@ -33,60 +33,47 @@ if (mysqli_num_rows($query2) > 0) {
 ?>
 
 <!-- profile -->
-<div class="profile_container">
-<img src="assets/pfp/<?php echo $row2['pfp'];?>" class="profile_image">
-<h1 class="profile_text_big"> <?php echo $row2['firstname'];?> <?php echo $row2['lastname'];?> </h1>
-<p class="profile_text_small"> <?php echo $row2['work'];?> - <?php echo $row2['year_xp'];?> year(s) of experience </p>
-<a href="profile_edit.php"><button id="edit" class="btn btn-primary">Edit Profile</button></a>
-</div>
-
-<div class="about_container">
-<h1>Experience</h1>
-<p> <?php echo $row2['xp'];?> </p>
-<hr>
-<h1>About me</h1>
-<p> <?php echo $row2['aboutme'];?> </p>
-</div>
-
-
-
-
-<div class="skill_container_one">
-
-<h4>Skills</h4>
-<?php while ($row = mysqli_fetch_array($query2)) { 
-   ?>
-<p class="skill"> <?php echo $row4['skills'];?> </p>
-<?php } ?>
-<h4>Location</h4>
-<p> <?php echo $row2['location'];?> </p>
-<h4>Website</h4>
-<a href="https://<?php echo $row2['website'];?>" target="_blank"><?php echo $row2['website'];?></a>
-<h4>E-mail adress</h4>
-<p> <?php echo $row2['email'];?> </p>
-</div>
-
-<div class="skill_container_two">
-<h3> Work experience </h3>
-
-
-<?php mysqli_data_seek($query2, 0); ?>
-<?php while ($row = mysqli_fetch_array($query2)) { ?>
-<img class="work_logo" src="assets/pfp/<?php echo $row['work_pf'];?>">
-<h5> <?php echo $row['work_info'];?> </h5>
-<h6> <?php echo $row['work_co'];?> </h6>
-<p> <?php echo $row['work_xp'];?> year(s) </p>
-<?php } ?>
-</div>
-
+  <div class="profile_container">
+    <img src="assets/pfp/<?php echo $row2['pfp'];?>" class="profile_image">
+    <h1 class="profile_text_big"> <?php echo $row2['firstname'];?> <?php echo $row2['lastname'];?> </h1>
+    <p class="profile_text_small"> <?php echo $row2['work'];?> - <?php echo $row2['year_xp'];?> year(s) of experience </p>
+    <a href="profile_edit.php"><button id="edit" class="btn btn-primary">Edit Profile</button></a>
+  </div>
+  <div class="about_container">
+    <h1>Experience</h1>
+    <p> <?php echo $row2['xp'];?> </p>
+    <hr>
+    <h1>About me</h1>
+    <p> <?php echo $row2['aboutme'];?> </p>
+  </div>
+  <div class="skill_container_one">
+    <h4>Skills</h4>
+    <?php while ($row = mysqli_fetch_array($query2)) { 
+      ?>
+    <p class="skill"> <?php echo $row4['skills'];?> </p>
+    <?php } ?>
+    <h4>Location</h4>
+    <p> <?php echo $row2['location'];?> </p>
+    <h4>Website</h4>
+    <a href="https://<?php echo $row2['website'];?>" target="_blank"><?php echo $row2['website'];?></a>
+    <h4>E-mail adress</h4>
+    <p> <?php echo $row2['email'];?> </p>
+  </div>
+  <div class="skill_container_two">
+    <h3> Work experience </h3>
+    <?php mysqli_data_seek($query2, 0); ?>
+    <?php while ($row = mysqli_fetch_array($query2)) { ?>
+    <img class="work_logo" src="assets/pfp/<?php echo $row['work_pf'];?>">
+    <h5> <?php echo $row['work_info'];?> </h5>
+    <h6> <?php echo $row['work_co'];?> </h6>
+    <p> <?php echo $row['work_xp'];?> year(s) </p>
+    <?php } ?>
+  </div>
 <?php
 } else {
   header('location: 404.php');
 }
 ?>
-
-
-
 </body>
 <script src="assets/js/usermenu.js"></script>
 </html>
